d8901dd13801a772bea7e65670085e6aac78f903
EloiMusk <elioteuber@outlook.com>
2021-10-07 16:03:43 +0200

    Frontend Done No actuall functionalities

---
+++ src/components/MainPage/MainPage.ts
@@ -0,0 +1,38 @@
+import {Component, Vue} from 'vue-property-decorator';
+
+interface form {
+    firstName: string,
+    lastName: string,
+    email: string,
+    mobile: string,
+    issue: issues,
+    issueText: string
+}
+interface option{
+    value: string | null,
+    text: string
+}
+enum issues {
+    Payment = 'Payment',
+    Accounting = 'Accounting',
+    Login = 'Login',
+    Legal_Matters = 'Legal matters',
+    other = 'other'
+}
+@Component({
+    components: {},
+})
+
+export default class MainPage extends Vue {
+    private form: form = {firstName: '', lastName: '', email: '', issue:issues.other, mobile: '', issueText: ''}
+    private issues: Array<option> = [
+        {value:'Payment',text:'Payment'},
+        {value: 'Accounting', text:'Accounting'},
+        {value:'Login',text:'Login'},
+        {value:'Legal_Matters',text:'Legal matters'},
+        {value:'other',text:'other'}
+    ]
+    submit(){
+        alert(this.form.firstName)
+    }
+}
---
+++ src/components/MainPage/MainPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="MainPage">
+    <b-card class="m-5">
+      <h1>Shopbase Support</h1>
+      <b-form class="row m-2" @submit="submit">
+        <label for="firstName">Firstname</label>
+        <b-form-input
+            id="firstName"
+            v-model="form.firstName"
+        ></b-form-input>
+        <label for="lastName">Lastname</label>
+        <b-form-input
+            id="lastName"
+            v-model="form.lastName"
+        ></b-form-input>
+        <label for="Email">Email</label>
+        <b-form-input
+            id="Email"
+            v-model="form.email"
+        ></b-form-input>
+        <label for="mobile">Mobile</label>
+        <b-form-input
+            id="mobile"
+            v-model="form.mobile"
+        ></b-form-input>
+        <label for="issue-type">Issue Type</label>
+        <b-form-select id="issue-type" class="form-select-lg mt-1" v-model="form.issue"
+                       :options="issues"></b-form-select>
+        <label for="issue-text">Issue Type</label>
+        <b-form-textarea
+            id="issue-text"
+            v-model="form.issueText"
+            rows="3"
+            class="mt-1"
+        >
+        </b-form-textarea>
+      </b-form>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts" src="./MainPage.ts"></script>
+
+<style>
+
+</style>
